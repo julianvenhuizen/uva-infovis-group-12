97d7e05a079da9fbc7e3c9448ad3d4153a7528cd
Alexandra Spruit <alexandra.spruit@hotmail.com>
2021-03-14 23:45:19 +0100

    javascript imported

--- app/static/js/europamap.js
+++ app/static/js/europamap.js
@@ -1,5 +1,5 @@
 alert("JAVASCRIPT IS IMPORTED.");
-var btn = document.getElementsByID("AT");
+var btn = document.getElementById("AT");
 btn.onclick = function () {
-    btn.style.fill = #E9860F;
+    btn.style.fill = "#E9860F";
 }
