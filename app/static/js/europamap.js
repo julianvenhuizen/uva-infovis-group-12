4254c3059356be0b74c1456b34c9399e0d37feba
Sam van der Voort <76652867+sam96master@users.noreply.github.com>
2021-03-17 19:33:51 +0100

    Added color changes / changed design / added selection feature

--- app/static/js/europamap.js
+++ app/static/js/europamap.js
@@ -1,5 +1,23 @@
-alert("JAVASCRIPT IS IMPORTED.");
-var btn = document.getElementById("AT");
-btn.onclick = function () {
-    btn.style.fill = "#E9860F";
+var selectedCountry = [];
+
+function changeColor(country) {
+    if (!selectedCountry.includes(country)) {
+        country.style.fill = "#E9860F";
+        selectedCountry = selectedCountry.concat(country);
+        console.log(selectedCountry);
+    }
+    else {
+        country.style.fill = "blue";
+        selectedCountry = arrayRemove(selectedCountry, country);
+    }
+
 }
+
+function arrayRemove(arr, value) {
+    return arr.filter(function(ele) {
+        return ele != value;
+    })
+};
+
+
+// console.log({{data|tojson}});
