31946c91bb924fb458100dd35aa51b462b07a667
RikBr <RikBr@users.noreply.github.com>
2021-03-18 01:25:03 +0100

    Country ID is now displayed and stored in array
    We now store the country ID instead of an svg object in the array. This ID can be used to filter the data
    
    Also took js code from home.html and put it in europamap.js

--- app/static/js/europamap.js
+++ app/static/js/europamap.js
@@ -1,23 +1,34 @@
-var selectedCountry = [];
+var selectedCountries = [];
 
+// Function that changes the color and adds country ID to the array
+// Also displays the first country in the array
 function changeColor(country) {
-    if (!selectedCountry.includes(country)) {
+    if (!selectedCountries.includes(country.id)) {
         country.style.fill = "#E9860F";
-        selectedCountry = selectedCountry.concat(country);
-        console.log(selectedCountry);
-    }
-    else {
-        country.style.fill = "blue";
-        selectedCountry = arrayRemove(selectedCountry, country);
-    }
+        selectedCountries = selectedCountries.concat(country.id);
 
+        // Display first country in array
+        document.getElementById("title_or_countryname").innerHTML = selectedCountries[0];
+    }
+    // If country is in array ("selected"): deselect and return to blue color
+    else {
+        country.style.fill = "#3386AE";
+
+        selectedCountries = arrayRemove(selectedCountries, country.id);
+        
+        // Display first country in array if any, else display some string
+        if (selectedCountries.length < 1) {
+            document.getElementById("title_or_countryname").innerHTML = "No country is selected";
+        }
+        else {
+            document.getElementById("title_or_countryname").innerHTML = selectedCountries[0];
+        }
+    }
 }
 
+// Function needed to deselect country, don't touch this please 
 function arrayRemove(arr, value) {
     return arr.filter(function(ele) {
         return ele != value;
     })
 };
-
-
-// console.log({{data|tojson}});
