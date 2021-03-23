baadfa339f68bdfdea61c2b7ed7ffe3371c82cdb
Alexandra Spruit <alexandra.spruit@hotmail.com>
2021-03-23 09:56:03 +0100

    Reformatting code, setting up side panel layout, adding country names dict, adding max select

--- app/static/js/europamap.js
+++ app/static/js/europamap.js
@@ -1,32 +1,23 @@
-// Function that changes the color and adds country ID to the array
-// Also displays the first country in the array
+// Function that changes the color and adds country ID to the selected countries array
+// Also updates the left column's information
 function changeColor(country) {
     if (!selectedCountries.includes(country.id)) {
-        country.classList.add("selected-country");
-        selectedCountries = selectedCountries.concat(country.id);
-        console.log(selectedCountries)
 
-        // Display last country in array
-        nItems = selectedCountries.length
-        document.getElementById("title_or_countryname").innerHTML = selectedCountries[nItems-1];
+        // Sets the selected country limit to three
+        if (selectedCountries.length > 2) {
+            alert("You may only select three countries at the same time.");
+        } else {
+            country.classList.add("selected-country");
+            selectedCountries = selectedCountries.concat(country.id);
+        }
     }
     // If country is in array ("selected"): deselect and return to blue color
     else {
         country.classList.remove("selected-country");
-
         selectedCountries = arrayRemove(selectedCountries, country.id);
-        
-        // Display last country in array if any, else display some string
-        if (selectedCountries.length < 1) {
-            document.getElementById("title_or_countryname").innerHTML = "No country is selected";
-        }
-        else {
-            nItems = selectedCountries.length
-            document.getElementById("title_or_countryname").innerHTML = selectedCountries[nItems-1];
-        }
     }
+    updateLastSelectedCountry();
     updateLeftColumn();
-    printCountryList();
 }
 
 // Function needed to deselect country, don't touch this please 
--- app/static/js/leftcolumn.js
+++ app/static/js/leftcolumn.js
@@ -1,20 +1,70 @@
 var selectedCountries = [];
+var lastSelectedCountry;
+var countryNames = {
+        "AL": "Albania",
+        "AT": "Austria",
+        "BE": "Belgium",
+        "BG": "Bulgaria",
+        "BA": "Bosnia and Herz.",
+        "BY": "Belarus",
+        "CH": "Switzerland",
+        "CZ": "Czech Rep.",
+        "DE": "Germany",
+        "DK": "Denmark",
+        "EE": "Estonia",
+        "FI": "Finland",
+        "GB": "United Kingdom",
+        "GR": "Greece",
+        "HR": "Croatia",
+        "HU": "Hungary",
+        "IE": "Ireland",
+        "IS": "Iceland",
+        "IT": "Italy",
+        "LT": "Lithuania",
+        "LU": "Luxembourg",
+        "LV": "Latvia",
+        "MD": "Moldova",
+        "MK": "Macedonia",
+        "ME": "Montenegro",
+        "NL": "Netherlands",
+        "NO": "Norway",
+        "PL": "Poland",
+        "PT": "Portugal",
+        "RO": "Romania",
+        "RS": "Serbia",
+        "SK": "Slovakia",
+        "SI": "Slovenia",
+        "SE": "Sweden",
+        "UA": "Ukraine",
+        "FR": "France",
+        "ES": "Spain"
+    };
 
 // Displays instructions when no country is selected and otherwise the graphs
 function updateLeftColumn() {
     var instructions = document.getElementById("instructions-panel");
     var graphs = document.getElementById("graphs-panel");
 
+    // Prints the home screen if there are no countries selected
     if (selectedCountries.length === 0) {
         console.log("Array is empty!")
 
+        document.getElementById("title_or_countryname").innerHTML = "Our Project";
+
         instructions.style.display = 'block';
         graphs.style.display = 'none';
+
+    // Prints the graphs corresponding to the selected countries
     } else {
         console.log(selectedCountries);
 
+
+        document.getElementById("title_or_countryname").innerHTML = countryNames[lastSelectedCountry];
+
         instructions.style.display = 'none';
         graphs.style.display = 'block';
+
+        printCountryList();
     }
 }
 
@@ -22,22 +72,22 @@ function updateLeftColumn() {
 // Prints the country list inside the barplot div NOT WORKING YET
 function printCountryList() {
 
-    listContainer = document.createElement('div'),
-    listElement = document.createElement('ul'),
-    numberOfListItems = selectedCountries.length, listItem, i;
+    // Start with a clean list element
+    var listElement = document.getElementById('selected-countries-list');
+    listElement.innerHTML = '';
 
-    // Add it to the page
-    document.getElementById('barplot').appendChild(listContainer);
-    listContainer.appendChild(listElement);
-
+    // Add selected countries to the list
+    var numberOfListItems = selectedCountries.length, listItem, i;
     for (i = 0; i < numberOfListItems; ++i) {
-        // create an item for each one
         listItem = document.createElement('li');
-
-        // Add the item text
+        console.log("list item: ");
+        console.log(listItem);
         listItem.innerHTML = selectedCountries[i];
-
-        // Add listItem to the listElement
         listElement.appendChild(listItem);
     }
+}
+
+function updateLastSelectedCountry() {
+    nItems = selectedCountries.length;
+    lastSelectedCountry = selectedCountries[nItems-1];
 }
