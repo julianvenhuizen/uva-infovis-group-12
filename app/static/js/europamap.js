d273e05783a3e29513a8606e882a77602d5c8256
Alexandra Spruit <alexandra.spruit@hotmail.com>
2021-03-23 10:32:10 +0100

    change function name

--- app/static/js/europamap.js
+++ app/static/js/europamap.js
@@ -1,11 +1,10 @@
 // Function that changes the color and adds country ID to the selected countries array
 // Also updates the left column's information
-function changeColor(country) {
+function selectCountry(country) {
     if (!selectedCountries.includes(country.id)) {
 
         // Sets the selected country limit to three
         if (selectedCountries.length > 2) {
-            alert("You may only select three countries at the same time.");
         } else {
             country.classList.add("selected-country");
             selectedCountries = selectedCountries.concat(country.id);
@@ -16,6 +15,8 @@ function changeColor(country) {
         country.classList.remove("selected-country");
         selectedCountries = arrayRemove(selectedCountries, country.id);
     }
+
+    // Updates variables, text and graphs according to the select
     updateLastSelectedCountry();
     updateLeftColumn();
 }
