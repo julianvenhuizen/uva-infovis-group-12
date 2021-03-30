db20a18a0af7e4c3319dd2951fd352ed27514964
RikBr <RikBr@users.noreply.github.com>
2021-03-30 14:00:22 +0200

    Fixed timeslider year display + small bug
    Fixed: Year now displays properly above timeslider
    
    Fixed: Changing the year without having a country selected now does not try to display a sunburst anymore. This used to give a weird change in background colour in the sunburst box

--- app/static/js/timeslider.js
+++ app/static/js/timeslider.js
@@ -3,6 +3,8 @@ var slider = document.getElementById("myRange");
 // Update variable year with current slider value and display (each time you drag the slider handle)
 slider.oninput = function() {
 	selectedYear = slider.value
-  	document.getElementsByClassName("yeardisplay").innerHTML = selectedYear;
+  	document.getElementById("yeardisplay").innerHTML = selectedYear;
+  	if (selectedCountries.length > 0) {
   	updateSunburst(selectedYear, lastSelectedCountry);
+	}
 }
