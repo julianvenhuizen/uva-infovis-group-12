bf19dc5937b85b0424791087944b0cbe3f8ed6d5
RikBr <RikBr@users.noreply.github.com>
2021-03-30 18:55:13 +0200

    Restructured reset.js code + comments

--- app/static/js/reset.js
+++ app/static/js/reset.js
@@ -1,27 +1,27 @@
 // Reset button js code
 
 function reset_board() {
+	// All countries that are colored orange
 	var orangecountries = document.getElementsByClassName("selected-country")
 
-	//console.log(orangecountries)
-
-	//orangecountries.classList.remove("selected-country");
-
+	// Make them blue again
 	while (orangecountries.length > 0) {
 	 orangecountries[0].classList.remove("selected-country");
 	 orangecountries = document.getElementsByClassName("selected-country")
 	 }
 
+	// Reset all global variables
 	selectedCountries = [];
 	FirstSelectedCountry;
 	selectedYear = 2019;
 	selectedBudget = "SUSTAINABLE GROWTH: NATURAL RESOURCES";
 
+	//Reset timeslider state
 	document.getElementById("yeardisplay").innerHTML = selectedYear;
 	var slider = document.getElementById("myRange");
-
 	slider.value = selectedYear
 
+	// Reset displayed country name and country dropdown, sunburst and barchart
 	updateLeftColumn();
 
 	updateSunburst(selectedYear, FirstSelectedCountry);
