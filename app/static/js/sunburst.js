31240db7b670bb76f620debf6f068d8b9cd57e5f
Sam van der Voort <76652867+sam96master@users.noreply.github.com>
2021-03-30 16:25:03 +0200

    Added half dict

--- app/static/js/sunburst.js
+++ app/static/js/sunburst.js
@@ -1,6 +1,11 @@
-const width = window.innerWidth,
-    height = window.innerHeight,
-    maxRadius = Math.min(width, height)/1.5;
+var abbrev = {
+    'ADMINISTRATION':'Admin'
+}
+
+
+const format = d3.format(",d");
+    const width = 360;
+    const radius = width / 6;
 
 function updateSunburst(year, country) {
   removeoldSunburst();
@@ -16,199 +21,124 @@ function createNewSunburst(year, country) {
     var sunburst_year_data_country = sunburst_data[year][country];
     console.log(sunburst_year_data_country);
 
-const formatNumber = d3.format(',d');
+    const arc = d3.arc()
+            .startAngle(d => d.x0)
+            .endAngle(d => d.x1)
+            .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+            .padRadius(radius * 1.5)
+            .innerRadius(d => d.y0 * radius)
+            .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
+    
+    const partition = data => {
+        const root = d3.hierarchy(data)
+                .sum(d => d.size)
+                .sort((a, b) => b.value - a.value);
+        return d3.partition()
+                .size([2 * Math.PI, root.height + 1])
+                (root);
+    }
+    
+    const {require} = new observablehq.Library;
 
-const x = d3
-  .scaleLinear()
-  .range([0, 2 * Math.PI])
-  .clamp(true);
-
-const y = d3.scaleSqrt().range([maxRadius * 0.1, maxRadius]);
-
-// sunlight style guide network colors
-// https://github.com/amycesal/dataviz-style-guide/blob/master/Sunlight-StyleGuide-DataViz.pdf
-const dark = [
-  '#B08B12',
-  '#BA5F06',
-  '#8C3B00',
-  '#6D191B',
-  '#842854',
-  '#5F7186',
-  '#193556',
-  '#137B80',
-  '#144847',
-  '#254E00'
-];
-
-const mid = [
-  '#E3BA22',
-  '#E58429',
-  '#BD2D28',
-  '#D15A86',
-  '#8E6C8A',
-  '#6B99A1',
-  '#42A5B3',
-  '#0F8C79',
-  '#6BBBA1',
-  '#5C8100'
-];
-
-const light = [
-  '#F2DA57',
-  '#F6B656',
-  '#E25A42',
-  '#DCBDCF',
-  '#B396AD',
-  '#B0CBDB',
-  '#33B6D0',
-  '#7ABFCC',
-  '#C8D7A1',
-  '#A0B700'
-];
-
-const palettes = [light, mid, dark];
-const lightGreenFirstPalette = palettes
-  .map(d => d.reverse())
-  .reduce((a, b) => a.concat(b));
-
-const color = d3.scaleOrdinal(lightGreenFirstPalette);
-
-const partition = d3.partition();
-
-const arc = d3
-  .arc()
-  .startAngle(d => x(d.x0))
-  .endAngle(d => x(d.x1))
-  .innerRadius(d => Math.max(0, y(d.y0)))
-  .outerRadius(d => Math.max(0, y(d.y1)));
-
-const middleArcLine = d => {
-  const halfPi = Math.PI / 2;
-  const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
-  const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-
-  const middleAngle = (angles[1] + angles[0]) / 2;
-  const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
-  if (invertDirection) {
-    angles.reverse();
-  }
-
-  const path = d3.path();
-  path.arc(0, 0, r, angles[0], angles[1], invertDirection);
-  return path.toString();
-};
-
-const textFits = d => {
-  const CHAR_SPACE = 6;
-
-  const deltaAngle = x(d.x1) - x(d.x0);
-  const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-  const perimeter = r * deltaAngle;
-
-  return d.data.name.length * CHAR_SPACE < perimeter;
-};
-
-d3.select("#sunburst-box")
-    .append("sunburst")
-    .attr("id", "sunburst")
-
-const svg = d3
-  .select('#sunburst')
-  .append('svg')
-  .style('width', '40vw')
-  .style('height', '60vh')
-  .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
-  .on('click', () => focusOn()); // Reset zoom on canvas click
-
-
-    root = d3.hierarchy(sunburst_year_data_country);
-    root.sum(d => d.size);
-
-    const slice = svg.selectAll('g.slice').data(partition(root).descendants());
-
-    slice.exit().remove();
-
-    const newSlice = slice
-      .enter()
-      .append('g')
-      .attr('class', 'slice')
-      .on('click', d => {
-        d3.event.stopPropagation();
-        focusOn(d);
-      });
-
-    newSlice
-      .append('title')
-      .text(d => d.data.name + '\n' + formatNumber(d.value));
-
-    newSlice
-      .append('path')
-      .attr('class', 'main-arc')
-      .style('fill', d => color((d.children ? d : d.parent).data.name))
-      .attr('d', arc);
-
-
-    const text = newSlice
-      .append('text')
-      .attr('display', d => (textFits(d) ? null : 'none'));
-
-    // Add white contour
-    text
-      .append('textPath')
-      .attr('startOffset', '50%')
-      .attr('xlink:href', (_, i) => `#main-arc${i}`)
-      .text(d => d.data.name)
-      .style('fill', 'none')
-      .style('stroke', '#E5E2E0')
-      .style('stroke-width', 12)
-      .style('stroke-linejoin', 'round');
-
-    text
-      .append('textPath')
-      .attr('startOffset', '50%')
-      .attr('xlink:href', (_, i) => `#main-arc${i}`)
-      .text(d => d.data.name);
-
-
-function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 }) {
-  // Reset to top-level if no data point specified
-
-  const transition = svg
-    .transition()
-    .duration(750)
-    .tween('scale', () => {
-      const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-        yd = d3.interpolate(y.domain(), [d.y0, 1]);
-      return t => {
-        x.domain(xd(t));
-        y.domain(yd(t));
-      };
-    });
-
-  transition.selectAll('path.main-arc').attrTween('d', d => () => arc(d));
-
-  transition
-    .selectAll('path.hidden-arc')
-    .attrTween('d', d => () => middleArcLine(d));
-
-  transition
-    .selectAll('text')
-    .attrTween('display', d => () => (textFits(d) ? null : 'none'));
-
-  moveStackToFront(d);
-
-  //
-
-  function moveStackToFront(elD) {
-    svg
-      .selectAll('.slice')
-      .filter(d => d === elD)
-      .each(function(d) {
-        this.parentNode.appendChild(this);
-        if (d.parent) {
-          moveStackToFront(d.parent);
+    require()('@observablehq/flare').then(data => {
+        console.log(data);
+        const root = partition(sunburst_year_data_country);
+        const color = d3.scaleOrdinal().range(d3.quantize(d3.interpolateRainbow, data.children.length));
+    
+        root.each(d => d.current = d);
+    
+        const svg = d3.select('#partitionSVG')
+                .style("width", "100%")
+                .style("height", "auto")
+                .style("font", "5px sans-serif");
+    
+        const g = svg.append("g")
+                .attr("id", 'sunburst')
+                .attr("transform", `translate(${width / 2},${width / 2})`);
+    
+        const path = g.append("g")
+                .selectAll("path")
+                .data(root.descendants().slice(1))
+                .join("path")
+                .attr("fill", d => {
+                    while (d.depth > 1)
+                        d = d.parent;
+                    return color(d.data.name);
+                })
+                .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+                .attr("d", d => arc(d.current));
+    
+        path.filter(d => d.children)
+                .style("cursor", "pointer")
+                .on("click", clicked);
+    
+        path.append("title")
+                .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+    
+        const label = g.append("g")
+                .attr("pointer-events", "none")
+                .attr("text-anchor", "middle")
+                .style("user-select", "none")
+                .selectAll("text")
+                .data(root.descendants().slice(1))
+                .join("text")
+                .attr("dy", "0.35em")
+                .attr("fill-opacity", d => +labelVisible(d.current))
+                .attr("transform", d => labelTransform(d.current))
+                .text(d => abbrev[d.data.name]);
+    
+        const parent = g.append("circle")
+                .datum(root)
+                .attr("r", radius)
+                .attr("fill", "none")
+                .attr("pointer-events", "all")
+                .on("click", clicked);
+    
+        function clicked(p) {
+            parent.datum(p.parent || root);
+    
+            root.each(d => d.target = {
+                    x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                    x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                    y0: Math.max(0, d.y0 - p.depth),
+                    y1: Math.max(0, d.y1 - p.depth)
+                });
+    
+            const t = g.transition().duration(750);
+    
+            // Transition the data on all arcs, even the ones that aren’t visible,
+            // so that if this transition is interrupted, entering arcs will start
+            // the next transition from the desired position.
+            path.transition(t)
+                    .tween("data", d => {
+                        const i = d3.interpolate(d.current, d.target);
+                        return t => d.current = i(t);
+                    })
+                    .filter(function (d) {
+                        return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+                    })
+                    .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+                    .attrTween("d", d => () => arc(d.current));
+    
+            label.filter(function (d) {
+                return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+            }).transition(t)
+                    .attr("fill-opacity", d => +labelVisible(d.target))
+                    .attrTween("transform", d => () => labelTransform(d.current));
         }
-      });
-  }
-}
+    
+        function arcVisible(d) {
+            return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+        }
+    
+        function labelVisible(d) {
+            return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+        }
+    
+        function labelTransform(d) {
+            const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+            const y = (d.y0 + d.y1) / 2 * radius;
+            return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+        }
+    });
 }
